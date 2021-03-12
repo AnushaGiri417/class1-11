a82f26cfd1a537ae9d20014a7e47ccf26e6c8481
AnushaGiri417 <anushagiri417@gmail.com>
2021-03-11 20:58:44 -0500

    Third commit

--- src/testTheGit/Git.java
+++ src/testTheGit/Git.java
@@ -3,7 +3,18 @@ package testTheGit;
 public class Git {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("This is git statement");
+		
+		System.out.println("We are all learning git");
+		
+		int a=1;
+		int b=2;
+		
+		System.out.println(a+b);
+		
+		
+		
 
 	}
 
