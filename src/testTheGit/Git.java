651da1875f30faf8f33ee25db29df26f9f823468
AnushaGiri417 <anushagiri417@gmail.com>
2021-03-11 21:13:53 -0500

    This is my fourth commit

--- src/testTheGit/Git.java
+++ src/testTheGit/Git.java
@@ -5,17 +5,11 @@ public class Git {
 	public static void main(String[] args) {
 		
 		System.out.println("This is git statement");
-		
 		System.out.println("We are all learning git");
-		
 		int a=1;
 		int b=2;
-		
 		System.out.println(a+b);
-		
-		
-		
-
+		System.out.println("Git is driving me crazy");
 	}
 
 }
---
+++ src/testTheGit/SecondGitClass.java
@@ -0,0 +1,15 @@
+package testTheGit;
+
+public class SecondGitClass {
+
+	public static void main(String[] args) {
+/*
+hello this is my line
+how are you all doing
+i am here writing some code
+hope it makes me understand Git better 
+*/
+		
+	}
+
+}
